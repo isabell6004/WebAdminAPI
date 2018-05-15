7247970cbe147732cc3ce7ab18cdb8b75d95602c
brianfashiongo <brian.kim@nhnglobal.com>
2018-05-15 11:33:04 -0700

    first commit

---
+++ src/main/java/net/fashiongo/webadmin/WebAdminAPIApplication.java
@@ -0,0 +1,11 @@
+package net.fashiongo.webadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdminAPIApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebAdminAPIApplication.class, args);
+	}
+}
