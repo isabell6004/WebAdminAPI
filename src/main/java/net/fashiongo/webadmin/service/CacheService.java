583f52bb03a1761dc10e1b18ead3264e22b5195b
incheol_KOR <incheol@fashiongo.com>
2018-09-18 14:45:09 +0900

    [Incheol] migration add fg_em datasource

---
+++ src/main/java/net/fashiongo/webadmin/service/CacheService.java
@@ -0,0 +1,33 @@
+package net.fashiongo.webadmin.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import net.fashiongo.webadmin.utility.HttpClient;
+import net.fashiongo.webadmin.utility.JsonResponse;
+
+/**
+ * 
+ * @author Incheol Jung
+ */
+@Service
+public class CacheService {
+	@Autowired
+	@Qualifier("serviceJsonClient")
+	private HttpClient httpClient;
+	
+	public JsonResponse GetRedisCacheEvict(String cacheName, String key) {
+		String request = "cache/".concat(cacheName).concat("/").concat(key);
+		JsonResponse result = httpClient.get(request);
+		
+		return result;
+	}
+	
+	public JsonResponse GetRedisCacheEvict_ChangeDirName(String oldDirName, String newDirName) {
+		String request = "misc/shell/changeDirname/".concat(oldDirName).concat("/").concat(newDirName);
+		JsonResponse result = httpClient.get(request);
+		
+		return result;
+	}
+}
--- src/main/java/net/fashiongo/webadmin/service/TestService.java
+++ src/main/java/net/fashiongo/webadmin/service/TestService.java
@@ -3,20 +3,27 @@ package net.fashiongo.webadmin.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import net.fashiongo.webadmin.dao.fgem.EmApplicationRepository;
+import net.fashiongo.webadmin.model.fgem.EmApplication;
 import net.fashiongo.webadmin.utility.HttpClient;
-import net.fashiongo.webadmin.utility.JsonResponse;
 
+/**
+ * 
+ * @author Incheol Jung
+ */
 @Service
 public class TestService {
 	
 	@Autowired
 	@Qualifier("webAdminJsonClient")
 	private HttpClient httpClient;
+	
+	@Autowired
+	EmApplicationRepository emApplicationRepository;
 
 	public String getMethod(String message) {
 		if(message.equals("error")) {
@@ -24,11 +31,14 @@ public class TestService {
 			arr.add("test");
 			arr.get(10);
 		}else {
-			JSONObject obj = new JSONObject();
-			obj.put("token", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlaWQiOiJTIiwidXNlcklkIjoxMDksInVzZXJuYW1lIjoia3JkZXYiLCJmdWxsbmFtZSI6IktvcmVhIERldmVsb3BlciIsImlwYWRkciI6IjEwMy4yNDMuMjAwLjEyIiwidXNlcmFnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzY4LjAuMzQ0MC4xMDYgU2FmYXJpLzUzNy4zNiIsIm5iZiI6MTUzNzIyODE3Mi4wLCJpYXQiOjE1MzcyMjgxNzIuMCwiZXhwIjoxNTM3MjcxMzcyLjB9.RC4DJ-xa_MiWsuLxRqCDc6BdpxCW8Ok1EZ1yqW_IhI1K4_iGGdPGrVdA_pEGB08E_8SOkZxUFU2ho1YYWvfb-A");
-			JsonResponse result = httpClient.postObject("Account/TokenCheck", obj);
-//			JsonResponse result = httpClient.get("Account/Test");
-			System.out.println("result.getMessage()" + result.getMessage());
+//			JSONObject obj = new JSONObject();
+//			obj.put("token", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJyb2xlaWQiOiJTIiwidXNlcklkIjoxMDksInVzZXJuYW1lIjoia3JkZXYiLCJmdWxsbmFtZSI6IktvcmVhIERldmVsb3BlciIsImlwYWRkciI6IjEwMy4yNDMuMjAwLjEyIiwidXNlcmFnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzY4LjAuMzQ0MC4xMDYgU2FmYXJpLzUzNy4zNiIsIm5iZiI6MTUzNzIyODE3Mi4wLCJpYXQiOjE1MzcyMjgxNzIuMCwiZXhwIjoxNTM3MjcxMzcyLjB9.RC4DJ-xa_MiWsuLxRqCDc6BdpxCW8Ok1EZ1yqW_IhI1K4_iGGdPGrVdA_pEGB08E_8SOkZxUFU2ho1YYWvfb-A");
+//			JsonResponse result = httpClient.postObject("Account/TokenCheck", obj);
+////			JsonResponse result = httpClient.get("Account/Test");
+//			System.out.println("result.getMessage()" + result.getMessage());
+			
+			EmApplication em = emApplicationRepository.findOne(1);
+			System.out.println("em.getName()" + em.getName());
 		}
 		
 		return "testService response message";
