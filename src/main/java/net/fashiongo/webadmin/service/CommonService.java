b56db9b8e7d9f8d764b1c79474da7cbfd3e3d579
daisydahye <dahyejeong@fashiongo.com>
2018-10-08 16:54:35 +0900

    [Dahye] migration Add Common

--- src/main/java/net/fashiongo/webadmin/service/CommonService.java
+++ src/main/java/net/fashiongo/webadmin/service/CommonService.java
@@ -2,6 +2,8 @@ package net.fashiongo.webadmin.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import net.fashiongo.webadmin.dao.primary.SecurityMenuRepository;
 import net.fashiongo.webadmin.model.primary.SecurityMenu;
@@ -20,6 +22,18 @@ public class CommonService extends ApiService {
 		SecurityMenu result = new SecurityMenu();
 		result = securityMenuRepository.findOneByRoutePath(pageName);
 		return result.getMenuID();
+	}
+	
+	public void GetServerHeartBeat() {
+		
+	}
+	
+	public void GetCountryStates() {
+		
+	}
+	
+	public void GetTopCategories() {
+		
 	}
 
 }
