74d1c86a93d4b341ab75716dfeffc981d6a384d6
incheol_KOR <incheol@fashiongo.com>
2018-10-17 09:25:57 +0900

    [Incheol] migration update param

--- src/main/java/net/fashiongo/webadmin/model/pojo/parameter/SetdeletesecuritygroupsParameter.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/parameter/SetdeletesecuritygroupsParameter.java
@@ -1,20 +1,19 @@
 package net.fashiongo.webadmin.model.pojo.parameter;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 
  * @author Incheol Jung
  */
 public class SetdeletesecuritygroupsParameter {
-	List<Integer> data;
+	String data;
 
 	public List<Integer> getData() {
-		return data;
+		return StringUtils.isEmpty(data) ? null : Arrays.stream(data.replaceAll("\\[(.*)\\]", "").split(",")).map(Integer::parseInt).collect(Collectors.toList());
 	}
-
-	public void setData(List<Integer> data) {
-		this.data = data;
-	}
-	
 }
