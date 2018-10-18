49ea894bae77a31fb19f3e9b2c6fe6016f52edea
daisydahye <dahyejeong@fashiongo.com>
2018-10-18 11:21:47 +0900

    [Dahye] migration Update JUnitTest

--- src/test/java/net/fashiongo/webadmin/service/AdminServiceTest.java
+++ src/test/java/net/fashiongo/webadmin/service/AdminServiceTest.java
@@ -1,14 +1,23 @@
-//package net.fashiongo.webadmin.service;
-//
-//import static org.junit.Assert.*;
-//
-//import org.junit.Test;
-//
-//public class AdminServiceTest {
-//
-//	@Test
-//	public void test() {
-////		fail("Not yet implemented");
-//	}
-//
-//}
+package net.fashiongo.webadmin.service;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ * 
+ * @author DAHYE
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdminServiceTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+	}
+
+}
