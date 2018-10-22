61bc3a98066b95be8d651481cfd0736c1f201b00
incheol_KOR <incheol@fashiongo.com>
2018-10-22 11:20:21 +0900

    [Incheol] migration update test

--- src/test/java/net/fashiongo/webadmin/service/BuyerServiceTest.java
+++ src/test/java/net/fashiongo/webadmin/service/BuyerServiceTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @author DAHYE
  *
  */
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class BuyerServiceTest {
 	@Autowired
