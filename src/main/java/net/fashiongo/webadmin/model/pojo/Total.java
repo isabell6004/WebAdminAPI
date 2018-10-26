c7669fe79436ef2a2bf83ac3f6da4171fa9c2e21
daisydahye <dahyejeong@fashiongo.com>
2018-10-26 14:34:07 +0900

    [Dahye] migration FWAM-173 Fix type error

--- src/main/java/net/fashiongo/webadmin/model/pojo/Total.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/Total.java
@@ -1,6 +1,9 @@
 package net.fashiongo.webadmin.model.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Total {
+	@JsonProperty("RecCnt")
 	private Integer recCnt;
 
 	public Integer getRecCnt() {
--- src/main/java/net/fashiongo/webadmin/model/pojo/VendorNews.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/VendorNews.java
@@ -1,15 +1,47 @@
 package net.fashiongo.webadmin.model.pojo;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.persistence.Column;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 public class VendorNews {
-	private Integer row;
+	@JsonProperty("row")
+	@Column(name = "row")
+	private Long row;
+	
+	@JsonProperty("NewsID")
+	@Column(name = "NewsID")
 	private Integer newsID;
+	
+	@JsonProperty("Recipient")
+	@Column(name = "Recipient")
 	private String recipient;
+	
+	@JsonProperty("NewsTitle")
+	@Column(name = "NewsTitle")
 	private String newsTitle;
-	private String fromDate;
-	private String toDate;
+	
+	@JsonProperty("FromDate")
+	@Column(name = "FromDate")
+	private LocalDateTime fromDate;
+	
+	@JsonProperty("ToDate")
+	@Column(name = "ToDate")
+	private LocalDateTime toDate;
+	
+	@JsonProperty("Active")
+	@Column(name = "Active")
 	private Boolean active;
+	
+	@JsonProperty("WholeSalerID")
+	@Column(name = "WholeSalerID")
 	private Integer wholeSalerID;
-	public Integer getRow() {
+	
+	public Long getRow() {
 		return row;
 	}
 	public Integer getNewsID() {
@@ -22,10 +54,16 @@ public class VendorNews {
 		return newsTitle;
 	}
 	public String getFromDate() {
-		return fromDate;
+		if (fromDate == null) return null;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String formattedFromDate = fromDate.format(formatter);				
+		return formattedFromDate;
 	}
 	public String getToDate() {
-		return toDate;
+		if (toDate == null) return null;
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String formattedToDate = toDate.format(formatter);				
+		return formattedToDate;
 	}
 	public Boolean getActive() {
 		return active;
@@ -33,7 +71,7 @@ public class VendorNews {
 	public Integer getWholeSalerID() {
 		return wholeSalerID;
 	}
-	public void setRow(Integer row) {
+	public void setRow(Long row) {
 		this.row = row;
 	}
 	public void setNewsID(Integer newsID) {
@@ -45,11 +83,22 @@ public class VendorNews {
 	public void setNewsTitle(String newsTitle) {
 		this.newsTitle = newsTitle;
 	}
-	public void setFromDate(String fromDate) {
+	public void setFromDate(LocalDateTime fromDate) {
 		this.fromDate = fromDate;
 	}
-	public void setToDate(String toDate) {
+	
+	public void setFromDate(String fromDate) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");	
+		this.fromDate = LocalDateTime.parse(fromDate, formatter);
+	}
+
+	public void setToDate(LocalDateTime toDate) {
 		this.toDate = toDate;
+	}
+	
+	public void setToDate(String toDate) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss");	
+		this.toDate = LocalDateTime.parse(toDate, formatter);
 	}
 	public void setActive(Boolean active) {
 		this.active = active;
