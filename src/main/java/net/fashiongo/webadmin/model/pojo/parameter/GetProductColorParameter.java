825764889a30deebf6ec9a047ca7e106f2fdce3d
incheol_KOR <incheol@fashiongo.com>
2018-10-29 13:49:55 +0900

    [Incheol] migration GetProdroductColors

---
+++ src/main/java/net/fashiongo/webadmin/model/pojo/parameter/GetProductColorParameter.java
@@ -0,0 +1,24 @@
+package net.fashiongo.webadmin.model.pojo.parameter;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * 
+ * @author Incheol Jung
+ */
+public class GetProductColorParameter implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@ApiModelProperty(required = false, example="123456")
+	private Integer productid;
+
+	public Integer getProductid() {
+		return productid;
+	}
+
+	public void setProductid(Integer productid) {
+		this.productid = productid;
+	}
+	
+}
