4cc34ea2ffe3e9277b6a4721999289a96a417289
jiwonkim <jiwonkim@fashiongo.com>
2018-10-29 14:25:28 +0900

    [jiwon] FWAM-186~190

---
+++ src/main/java/net/fashiongo/webadmin/model/pojo/VendorData1.java
@@ -0,0 +1,36 @@
+package net.fashiongo.webadmin.model.pojo;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.persistence.Column;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+/**
+ * 
+ * @author Jiwon Kim
+ */
+public class VendorData1 {
+
+
+	@JsonProperty("WholeSalerID")
+	@Column(name = "WholeSalerID")
+	private Integer wholeSalerID;
+	@JsonProperty("FICount")
+	@Column(name = "FICount")
+	private Integer fICount;
+	public Integer getWholeSalerID() {
+		return wholeSalerID;
+	}
+	public void setWholeSalerID(Integer wholeSalerID) {
+		this.wholeSalerID = wholeSalerID;
+	}
+	public Integer getfICount() {
+		return fICount;
+	}
+	public void setfICount(Integer fICount) {
+		this.fICount = fICount;
+	}
+	
+}
