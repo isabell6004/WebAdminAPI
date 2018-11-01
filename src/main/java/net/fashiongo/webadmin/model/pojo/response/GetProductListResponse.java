8f2aa7ef6e03165bcefddcb34a8406d21e4ec4b4
incheol_KOR <incheol@fashiongo.com>
2018-11-01 11:09:52 +0900

    [Incheol] migration add comments

--- src/main/java/net/fashiongo/webadmin/model/pojo/response/GetProductListResponse.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/response/GetProductListResponse.java
@@ -6,7 +6,10 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import net.fashiongo.webadmin.model.pojo.ProductSummary;
-
+/**
+ * 
+ * @author Incheol Jung
+ */
 public class GetProductListResponse implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
