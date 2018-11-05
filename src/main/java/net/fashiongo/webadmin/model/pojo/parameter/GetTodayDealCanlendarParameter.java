a27c9c90daeb69d4d632643b83b414dc5ac29787
incheol_KOR <incheol@fashiongo.com>
2018-11-05 13:55:10 +0900

    [Incheol] migration update parameter

--- src/main/java/net/fashiongo/webadmin/model/pojo/parameter/GetTodayDealCanlendarParameter.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/parameter/GetTodayDealCanlendarParameter.java
@@ -1,9 +1,9 @@
 package net.fashiongo.webadmin.model.pojo.parameter;
 
 import java.io.Serializable;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 import org.apache.commons.lang3.StringUtils;
@@ -17,19 +17,25 @@ public class GetTodayDealCanlendarParameter implements Serializable {
 	@ApiModelProperty(required = false, example="2018-9-30")
 	private String todate;
 	
-	public String getFromdate() {
-//		DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy-M-d");
-//		return StringUtils.isNotEmpty(this.fromdate) ? LocalDateTime.now().minusDays(20) : null;
-		return this.fromdate;
+	public Date getFromdate() {
+		SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-d",Locale.US);
+		try {
+			return StringUtils.isNotEmpty(this.fromdate) ? dt.parse(this.fromdate) : null;
+		} catch (ParseException e) {
+			return null;
+		}
 	}
 	public void setFromdate(String fromdate) {
 		this.fromdate = fromdate;
-		
 	}
-	public String getTodate() {
-//		DateTimeFormatter dtFormatter = DateTimeFormatter.ofPattern("yyyy-M-d");
-//		return StringUtils.isNotEmpty(this.fromdate) ? LocalDateTime.now() : null;
-		return this.todate;
+	
+	public Date getTodate() {
+		SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-d",Locale.US);
+		try {
+			return StringUtils.isNotEmpty(this.todate) ? dt.parse(this.todate) : null;
+		} catch (ParseException e) {
+			return null;
+		}
 	}
 	public void setTodate(String todate) {
 		this.todate = todate;
