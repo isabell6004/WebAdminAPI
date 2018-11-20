c7c64f3ed9877ad194d4b9d5740dd24da5b93fbb
daisydahye <dahyejeong@fashiongo.com>
2018-11-20 16:09:59 +0900

    [Dahye] migration payment

--- src/main/java/net/fashiongo/webadmin/controller/WAPaymentController.java
+++ src/main/java/net/fashiongo/webadmin/controller/WAPaymentController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import net.fashiongo.webadmin.model.pojo.payment.response.GetPaymentStatusSearchOptionResponse;
 import net.fashiongo.webadmin.service.WAPaymentService;
+import net.fashiongo.webadmin.utility.JsonResponse;
 
 /**
  * 
@@ -18,6 +19,7 @@ import net.fashiongo.webadmin.service.WAPaymentService;
 public class WAPaymentController {
 	@Autowired
 	private WAPaymentService waPaymentService;
+	
 	/**
 	 * getPaymentStatusSearchOption
 	 * 
@@ -27,9 +29,107 @@ public class WAPaymentController {
 	 * @return GetPaymentStatusSearchOptionResponse
 	 */
 	@RequestMapping(value = "getPaymentStatusSearchOption", method = RequestMethod.POST)
-	public net.fashiongo.webadmin.utility.JsonResponse<GetPaymentStatusSearchOptionResponse> getPaymentStatusSearchOption() {
+	public JsonResponse<GetPaymentStatusSearchOptionResponse> getPaymentStatusSearchOption() {
 		GetPaymentStatusSearchOptionResponse result = waPaymentService.getPaymentStatusSearchOption();
 		
-		return new net.fashiongo.webadmin.utility.JsonResponse<GetPaymentStatusSearchOptionResponse>(true, null, 0, result);
+		return new JsonResponse<GetPaymentStatusSearchOptionResponse>(true, null, 0, result);
+	}
+	
+	/**
+	 * getPaymentStatusList
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "getPaymentStatusList", method = RequestMethod.POST)
+	public JsonResponse<String> getPaymentStatusList() {
+		
+		return new JsonResponse<>();
+	}
+	
+	/**
+	 * GetPendingPaymentTransaction
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "getpendingpaymenttransaction", method = RequestMethod.POST)
+	public JsonResponse<String> getPendingPaymentTransaction() {
+		
+		return new JsonResponse<>();
+	}
+	
+	/**
+	 * GetCreditCardType
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "getCreditCardType", method = RequestMethod.POST)
+	public JsonResponse<String> getCreditCardType() {
+		
+		return new JsonResponse<>();
+	}
+	
+	/**
+	 * getCreditCardStatus
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "getCreditCardStatus", method = RequestMethod.POST)
+	public JsonResponse<String> getCreditCardStatus() {
+		
+		return new JsonResponse<>();
+	}
+	
+	/**
+	 * getAllSavedCreditCardInfo
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "getAllSavedCreditCardInfo", method = RequestMethod.POST)
+	public JsonResponse<String> getAllSavedCreditCardInfo() {
+		
+		return new JsonResponse<>();
+	}
+	
+	/**
+	 * SetRestorePendingPaymentTransaction
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "setrestorependingpaymenttransaction", method = RequestMethod.POST)
+	public JsonResponse<String> setRestorePendingPaymentTransaction() {
+		
+		return new JsonResponse<>();
+	}
+	
+	/**
+	 * GetPayoutHistory
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	@RequestMapping(value = "getpayouthistory", method = RequestMethod.POST)
+	public JsonResponse<String> getPayoutHistory() {
+		
+		return new JsonResponse<>();
 	}
 }
--- src/main/java/net/fashiongo/webadmin/service/WAPaymentService.java
+++ src/main/java/net/fashiongo/webadmin/service/WAPaymentService.java
@@ -4,10 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import net.fashiongo.webadmin.model.pojo.payment.response.GetPaymentStatusSearchOptionResponse;
 import net.fashiongo.webadmin.model.primary.PaymentStatus;
 import net.fashiongo.webadmin.model.primary.VendorCompany;
+import net.fashiongo.webadmin.utility.JsonResponse;
 
 /**
  * 
@@ -33,5 +36,97 @@ public class WAPaymentService extends ApiService {
 		result.setVendorList((List<VendorCompany>) _results.get(0));
 		result.setPaymentStatusList((List<PaymentStatus>) _results.get(1));
 		return result;
+	}
+	
+	
+	/**
+	 * getPaymentStatusList
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void getPaymentStatusList() {
+		
+		
+	}
+	
+	/**
+	 * GetPendingPaymentTransaction
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void getPendingPaymentTransaction() {
+		
+		
+	}
+	
+	/**
+	 * GetCreditCardType
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void getCreditCardType() {
+		
+		
+	}
+	
+	/**
+	 * getCreditCardStatus
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void getCreditCardStatus() {
+		
+		
+	}
+	
+	/**
+	 * getAllSavedCreditCardInfo
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void getAllSavedCreditCardInfo() {
+		
+		
+	}
+	
+	/**
+	 * SetRestorePendingPaymentTransaction
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void setRestorePendingPaymentTransaction() {
+		
+		
+	}
+	
+	/**
+	 * GetPayoutHistory
+	 * 
+	 * @since 2018. 11. 20.
+	 * @author Dahye
+	 * @param 
+	 * @return 
+	 */
+	public void getPayoutHistory() {
+		
+		
 	}
 }
