983193b7c350c7673f9634059262b364ee4b63e0
Andy Min <andy.min@nhnglobal.com>
2019-01-11 17:26:39 -0800

    [Andy] feature/photo-studio: removde confict files

--- src/main/java/net/fashiongo/webadmin/controller/VendorController.java
+++ src/main/java/net/fashiongo/webadmin/controller/VendorController.java
@@ -2,21 +2,16 @@ package net.fashiongo.webadmin.controller;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import net.fashiongo.webadmin.model.primary.VendorAutocomplete;
-import net.fashiongo.webadmin.model.pojo.parameter.GetProductColorParameter;
-import net.fashiongo.webadmin.model.pojo.parameter.GetProductListParameter;
-import net.fashiongo.webadmin.model.pojo.response.GetProductColorResponse;
-import net.fashiongo.webadmin.model.pojo.response.GetProductListResponse;
+import net.fashiongo.webadmin.model.pojo.vendor.ProductColor;
+import net.fashiongo.webadmin.model.pojo.vendor.parameter.GetProductColorParameter;
+import net.fashiongo.webadmin.model.pojo.vendor.parameter.GetProductListParameter;
+import net.fashiongo.webadmin.model.pojo.vendor.response.GetProductListResponse;
 import net.fashiongo.webadmin.model.primary.VendorCompany;
 import net.fashiongo.webadmin.service.VendorService;
 import net.fashiongo.webadmin.utility.JsonResponse;
@@ -25,44 +20,24 @@ import net.fashiongo.webadmin.utility.JsonResponse;
  * @author roy
  */
 @RestController
-@RequestMapping(value = "/vendor", produces = "application/json")
+@RequestMapping(value="/vendor", produces = "application/json")
 public class VendorController {
-
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
+	
 	@Autowired
 	VendorService vendorService;
-
+	
 	/**
 	 * Get vendor list
-	 * 
 	 * @since 2018. 10. 15.
 	 * @author roy
 	 * @return vendor list
 	 */
-	@RequestMapping(value = "getvendorlistall", method = RequestMethod.POST)
+	@RequestMapping(value="getvendorlistall", method=RequestMethod.POST)
 	public JsonResponse<List<VendorCompany>> getVendorListAll() {
-
-		List<VendorCompany> vendors = vendorService.getVendorList();
+		
+		List<VendorCompany> vendors = vendorService.getVendorList();	
 		return new JsonResponse<List<VendorCompany>>(true, null, 0, vendors);
 	}
-
-	@GetMapping(value = "/autocomplete/{prefix:.+}")
-	public JsonResponse<List<VendorAutocomplete>> getVendorsAutoomplete(@PathVariable("prefix") String prefix) {
-		JsonResponse<List<VendorAutocomplete>> response = new JsonResponse<>(false, null, null);
-
-		try {
-			List<VendorAutocomplete> vendors = vendorService.getVendorsAutoomplete(prefix);
-			response.setSuccess(true);
-			response.setData(vendors);
-		} catch (Exception ex) {
-			logger.error("Exception Error: ", ex);
-			response.setMessage(ex.getMessage());
-		}
-
-		return response;
-	}
-
 	
 	/**
 	 * 
@@ -84,10 +59,10 @@ public class VendorController {
 	}
 	
 	@RequestMapping(value="getproductcolor", method=RequestMethod.POST)
-	public JsonResponse<GetProductColorResponse> getProductColor(@RequestBody GetProductColorParameter parameters) {
-		JsonResponse<GetProductColorResponse> result = new JsonResponse<GetProductColorResponse>(true, null, null);
+	public JsonResponse<List<ProductColor>> getProductColor(@RequestBody GetProductColorParameter parameters) {
+		JsonResponse<List<ProductColor>> result = new JsonResponse<List<ProductColor>>(true, null, null);
 		
-		GetProductColorResponse _result = vendorService.getProductColor(parameters.getProductid());	
+		List<ProductColor> _result = vendorService.getProductColor(parameters.getProductid());	
 		result.setData(_result);
 		
 		return result; 
