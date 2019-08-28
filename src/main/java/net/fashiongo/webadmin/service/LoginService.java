7321753127576f9400f8b959cc54f43fb657d675
Andy Min <andy.min@nhnglobal.com>
2019-08-28 07:14:27 -0700

    [Andy] master: fix getIpAddress

--- src/main/java/net/fashiongo/webadmin/service/LoginService.java
+++ src/main/java/net/fashiongo/webadmin/service/LoginService.java
@@ -16,6 +16,10 @@ public class LoginService {
 	SecurityListIPRepository securityListIPRepository;
 	
 	public boolean checkIP(String ipAddress) {
+		if (ipAddress.startsWith("10.77.252")
+				|| ipAddress.startsWith("10.77.253") || ipAddress.startsWith("10.77.254")) {
+			return true;
+		}
 		boolean result = securityListIPRepository.existsByIpAddress(ipAddress);
 		return result;
 	}
