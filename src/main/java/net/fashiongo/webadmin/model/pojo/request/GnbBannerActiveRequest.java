85832e5fc72d921b3b2bef763f97963e3b00157f
Jeongil Won <jeongil.won@nhn.com>
2019-10-18 15:24:21 +0900

    [jeongil] feature/top-menu-renewal: fix request object

--- src/main/java/net/fashiongo/webadmin/model/pojo/request/GnbBannerActiveRequest.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/request/GnbBannerActiveRequest.java
@@ -1,5 +1,6 @@
 package net.fashiongo.webadmin.model.pojo.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Getter
 public class GnbBannerActiveRequest {
+	@JsonProperty("isActive")
 	private boolean isActive;
 }
