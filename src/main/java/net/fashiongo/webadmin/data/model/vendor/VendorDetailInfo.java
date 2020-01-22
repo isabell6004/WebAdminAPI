c6fc0baea02baccf4d0e451ec638d1907bbd29aa
mintakoh <mintak.oh@nhn.com>
2020-01-22 13:42:38 +0900

    [Mintak Oh] renewal/221: fix #FG-WEBADMIN-API-RENEWAL/221
    add default constructor to VendorDetailInfo

--- src/main/java/net/fashiongo/webadmin/data/model/vendor/VendorDetailInfo.java
+++ src/main/java/net/fashiongo/webadmin/data/model/vendor/VendorDetailInfo.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class VendorDetailInfo {
     @JsonProperty(value = "WholeSalerID")
     private Integer wholeSalerID;
