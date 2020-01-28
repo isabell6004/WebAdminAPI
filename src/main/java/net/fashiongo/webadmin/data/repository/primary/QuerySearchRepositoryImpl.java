8ff1e4bec19508cdea4b18d1a8f585b14c3c5aac
mintakoh <mintak.oh@nhn.com>
2020-01-28 14:13:06 +0900

    [Mintak Oh] renewal/223: fix #FG-WEBADMIN-API-RENEWAL/223
    modify query

--- src/main/java/net/fashiongo/webadmin/data/repository/primary/QuerySearchRepositoryImpl.java
+++ src/main/java/net/fashiongo/webadmin/data/repository/primary/QuerySearchRepositoryImpl.java
@@ -5,7 +5,8 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.NumberPath;
-import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.core.types.dsl.SimplePath;
+import com.querydsl.core.types.dsl.StringPath;
 import com.querydsl.jpa.sql.JPASQLQuery;
 import com.querydsl.sql.SQLExpressions;
 import net.fashiongo.webadmin.data.entity.primary.QWebAdminSearchQuerySearchIDEntity;
@@ -14,6 +15,7 @@ import net.fashiongo.webadmin.data.model.statistics.HotSearch;
 import net.fashiongo.webadmin.utility.MSSQLServer2012Templates;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -27,6 +29,7 @@ public class QuerySearchRepositoryImpl implements QuerySearchRepository {
 
 
     @Override
+    @Transactional
     public List<HotSearch> getHotSearch(Integer top, LocalDateTime fromDate, LocalDateTime toDate, String orderBy, String searchfield, String searchkeyword) {
         String searchQuery = "";
         int rank = 0;
@@ -43,6 +46,7 @@ public class QuerySearchRepositoryImpl implements QuerySearchRepository {
         }
 
         JPASQLQuery<HotSearch> query = new JPASQLQuery<HotSearch>(entityManager, new MSSQLServer2012Templates());
+        JPASQLQuery subquery = new JPASQLQuery(entityManager, new MSSQLServer2012Templates());
 
         Expression<Integer> constant = Expressions.constant(1);
         BooleanExpression filter = Expressions.asNumber(1).eq(constant);
@@ -61,26 +65,16 @@ public class QuerySearchRepositoryImpl implements QuerySearchRepository {
             }
 
             NumberPath<Long> pathUN_rankno = Expressions.numberPath(Long.class, "rankno");
-            NumberPath<Integer> pathUN_count = Expressions.numberPath(Integer.class, "CountSum");
 
-            if (frequency != 0) {
-                filter = filter.and(pathUN_count.goe(frequency));
-            }
-            if (rank != 0) {
-                filter = filter.and(pathUN_rankno.loe(0));
-            }
-
-            query.select(Projections.constructor(HotSearch.class,
+            subquery.select(
                     UN.searchQuery,
                     UN.userNameCount.sum().as("CountSum"),
                     SQLExpressions.rank().over().orderBy(UN.userNameCount.sum().desc()).as("rankno")
-                    ))
-                    .from(UN)
-                    .where(filter)
-                    .groupBy(UN.searchQuery)
-                    .having(UN.userNameCount.sum().goe(0))
-                    .orderBy(pathUN_rankno.asc());
-
+            ).from(UN)
+            .where(filter)
+            .groupBy(UN.searchQuery)
+            .orderBy(pathUN_rankno.asc())
+            .offset(0).limit(top);
         } else {
             QWebAdminSearchQuerySearchIDEntity SI = QWebAdminSearchQuerySearchIDEntity.webAdminSearchQuerySearchIDEntity;
 
@@ -95,28 +89,28 @@ public class QuerySearchRepositoryImpl implements QuerySearchRepository {
             }
 
             NumberPath<Long> pathSI_rankno = Expressions.numberPath(Long.class, "rankno");
-            NumberPath<Integer> pathSI_count = Expressions.numberPath(Integer.class, "CountSum");
 
-            if (frequency != 0) {
-                filter = filter.and(pathSI_count.goe(frequency));
-            }
-            if (rank != 0) {
-                filter = filter.and(pathSI_rankno.loe(0));
-            }
-
-            query.select(Projections.constructor(HotSearch.class,
+            subquery.select(
                     SI.searchQuery,
                     SI.searchIDCount.sum().as("CountSum"),
                     SQLExpressions.rank().over().orderBy(SI.searchIDCount.sum().desc()).as("rankno")
-            ))
-                    .from(SI)
-                    .where(filter)
-                    .groupBy(SI.searchQuery)
-                    .having(SI.searchIDCount.sum().goe(0))
-                    .orderBy(pathSI_rankno.asc());
+            ).from(SI)
+            .where(filter)
+            .groupBy(SI.searchQuery)
+            .orderBy(pathSI_rankno.asc())
+            .offset(0).limit(top);
         }
 
-        query.offset(0).limit(top);
+        SimplePath<Object> pathX = Expressions.path(Object.class, "X");
+        NumberPath<Long> pathX_rankno = Expressions.numberPath(Long.class, "rankno");
+        NumberPath<Integer> pathX_count = Expressions.numberPath(Integer.class, "CountSum");
+        StringPath pathX_SearchQuery = Expressions.stringPath("searchQuery");
+
+        query.select(Projections.constructor(HotSearch.class,
+                pathX_SearchQuery,
+                pathX_count,
+                pathX_rankno
+        )).from(subquery, pathX);
 
         return query.fetch();
     }
