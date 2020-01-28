29328cd5b1ad0f4f404118a4f18f611e160c8bee
mintakoh <mintak.oh@nhn.com>
2020-01-29 08:24:08 +0900

    [Mintak Oh] hotfix/renewal-217: hotfix #FG-WEBADMIN-API-RENEWAL/217

--- src/main/java/net/fashiongo/webadmin/data/model/vendor/VendorAdminAccount.java
+++ src/main/java/net/fashiongo/webadmin/data/model/vendor/VendorAdminAccount.java
@@ -3,11 +3,13 @@ package net.fashiongo.webadmin.data.model.vendor;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 public class VendorAdminAccount {
     @JsonProperty(value = "CreatedOn")
     private LocalDateTime createdOn;
@@ -23,6 +25,14 @@ public class VendorAdminAccount {
 
     @JsonProperty(value = "LastActivityDate")
     private LocalDateTime lastActivityDate;
+
+    public VendorAdminAccount(Timestamp createdOn, String userID, String userName, String userType, Timestamp lastActivityDate) {
+        this.createdOn = createdOn.toLocalDateTime();
+        this.userID = userID;
+        this.userName = userName;
+        this.userType = userType;
+        this.lastActivityDate = lastActivityDate.toLocalDateTime();
+    }
 
     public VendorAdminAccount(LocalDateTime createdOn, String userID, String userName, String userType, Timestamp lastActivityDate) {
         this.createdOn = createdOn;
