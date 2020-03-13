91eea6bebdd388ea515d7cd551eee9b2c770cc3d
isabell6004 <isabell.yuk@nhnglobal.com>
2020-03-13 10:10:00 -0700

    remove getter/setter

--- src/main/java/net/fashiongo/webadmin/data/model/buyer/AdminRetailer.java
+++ src/main/java/net/fashiongo/webadmin/data/model/buyer/AdminRetailer.java
@@ -1,14 +1,10 @@
 package net.fashiongo.webadmin.data.model.buyer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import lombok.Setter;
 
 import javax.persistence.Column;
 import java.time.LocalDateTime;
 
-@Getter
-@Setter
 public class AdminRetailer {
 
 	@Column(name = "Active")
