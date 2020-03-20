31b40705def1a3ca1fea9db1813d7ebbe89a3d5c
Soohyun <soohyun.lee@nhnglobal.com>
2020-03-19 20:10:44 -0700

    [soohyun] #FGM/437 log all activities resolved

--- src/main/java/net/fashiongo/webadmin/service/LoggingServiceImpl.java
+++ src/main/java/net/fashiongo/webadmin/service/LoggingServiceImpl.java
@@ -8,6 +8,8 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,36 +17,47 @@ import org.springframework.stereotype.Component;
 
 import net.fashiongo.webadmin.utility.Utility;
 
-
 @Component
 @Slf4j
 public class LoggingServiceImpl implements LoggingService {
 
-    //info logger
+     //info logger
     private final static Logger logger = LoggerFactory.getLogger("webadminAccessPayloadLogger");
 
     @Override
     public void logRequest(HttpServletRequest httpServletRequest, Object body) {
-        StringBuilder stringBuilder = new StringBuilder();
-        Map<String, String> parameters = buildParametersMap(httpServletRequest);
-
-        stringBuilder.append("REQUEST ");
-        stringBuilder.append("username=[").append(Utility.getUsername()).append("] ");
-        stringBuilder.append("ipaddress=[").append(Utility.getIpAddress(httpServletRequest)).append("] ");
-        stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
-        stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
-        stringBuilder.append("headers=[").append(buildHeadersMap(httpServletRequest)).append("] ");
-        
-        if (!parameters.isEmpty()) {
-            stringBuilder.append("parameters=[").append(parameters).append("] ");
-        }
-        
-        if (body != null) {
-            stringBuilder.append("body=[" + body + "]");
-        }
 
         if (!httpServletRequest.getRequestURI().contains("getserverheartbeat")) {
-            log.info(stringBuilder.toString());
+
+            StringBuilder stringBuilder = new StringBuilder();
+            Map<String, String> parameters = buildParametersMap(httpServletRequest);
+
+            stringBuilder.append("REQUEST ");
+            stringBuilder.append("username=[").append(Utility.getUsername()).append("] ");
+            stringBuilder.append("ipaddress=[").append(Utility.getIpAddress(httpServletRequest)).append("] ");
+            stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
+            stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
+            stringBuilder.append("useragent=[").append(httpServletRequest.getHeader("User-Agent")).append("] ");
+            //stringBuilder.append("headers=[").append(buildHeadersMap(httpServletRequest)).append("] ");
+        
+            if (!parameters.isEmpty()) {
+                stringBuilder.append("parameters=[").append(parameters).append("] ");
+            }
+        
+            if (body != null) {
+                //stringBuilder.append("body=[" + body + "]");
+                ObjectMapper objectMapper = new ObjectMapper();
+                String returnStr = "";
+                try {
+                    returnStr = objectMapper.writeValueAsString(body);
+                } catch (JsonProcessingException e) {
+                    returnStr = "";
+                    log.error(e.getMessage());
+                }
+                stringBuilder.append("body=" + body + "[").append(returnStr).append("]");
+            }
+
+            //log.info(stringBuilder.toString());
             logger.info(stringBuilder.toString());
         }
     }
@@ -52,6 +65,7 @@ public class LoggingServiceImpl implements LoggingService {
     @Override
     public void logResponse(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object body) {
 
+        if (!httpServletRequest.getRequestURI().contains("getserverheartbeat")) {
             StringBuilder stringBuilder = new StringBuilder();
 
             stringBuilder.append("RESPONSE ");
@@ -59,15 +73,16 @@ public class LoggingServiceImpl implements LoggingService {
             stringBuilder.append("ipaddress=[").append(Utility.getIpAddress(httpServletRequest)).append("] ");
             stringBuilder.append("method=[").append(httpServletRequest.getMethod()).append("] ");
             stringBuilder.append("path=[").append(httpServletRequest.getRequestURI()).append("] ");
-            stringBuilder.append("responseHeaders=[").append(buildHeadersMap(httpServletResponse)).append("] ");
-            stringBuilder.append("responseBody=[").append(body).append("] ");
+            stringBuilder.append("useragent=[").append(httpServletRequest.getHeader("User-Agent")).append("] ");
+            //stringBuilder.append("responseHeaders=[").append(buildHeadersMap(httpServletResponse)).append("] ");
+            //stringBuilder.append("responseBody=[").append(body).append("] ");
 
-        if (!httpServletRequest.getRequestURI().contains("getserverheartbeat")) {
-            log.info(stringBuilder.toString());
+            //log.info(stringBuilder.toString());
             logger.info(stringBuilder.toString());
         }
     }
-    
+
+
     private Map<String, String> buildParametersMap(HttpServletRequest httpServletRequest) {
         Map<String, String> resultMap = new HashMap<>();
         Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
