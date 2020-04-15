e3f2cd0db6ebb5c65a44d1fdb0356329f9a50b0e
Kevin Kwon <smk.zera@gmail.com>
2020-04-14 17:50:33 -0700

    [kevin] feature/fix_vendor_image

--- src/main/java/net/fashiongo/webadmin/model/ads/response/VerifyVendorImageResponse.java
+++ src/main/java/net/fashiongo/webadmin/model/ads/response/VerifyVendorImageResponse.java
@@ -4,11 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Getter;
-import net.fashiongo.webadmin.data.entity.primary.VendorImageRequestEntity;
-import org.springframework.util.CollectionUtils;
-
-import java.util.Comparator;
-import java.util.List;
 
 @Getter
 public class VerifyVendorImageResponse {
@@ -35,24 +30,6 @@ public class VerifyVendorImageResponse {
         response.vendorImageType = vendorImageType;
         response.approved = approved;
         response.active = active;
-
-        return response;
-    }
-
-    public static VerifyVendorImageResponse from(List<VendorImageRequestEntity> vendorImageRequestEntities) {
-
-        if (CollectionUtils.isEmpty(vendorImageRequestEntities))
-            throw new IllegalArgumentException("argument is null");
-
-        VerifyVendorImageResponse response = new VerifyVendorImageResponse();
-        VendorImageRequestEntity vendorImageRequestEntity = vendorImageRequestEntities.stream()
-                .max(Comparator.comparing(VendorImageRequestEntity::getImageRequestId))
-                .get();
-
-        response.vendorId = vendorImageRequestEntity.getWholesalerId();
-        response.vendorImageType = vendorImageRequestEntity.getVendorImageTypeId();
-        response.approved = vendorImageRequestEntity.getIsApproved();
-        response.active = vendorImageRequestEntity.isActive();
 
         return response;
     }
