f40622f04476f8aa839533aa05849708111e9527
Andy Min <andy.min@nhnglobal.com>
2020-04-20 18:28:39 -0700

    [Andy] security/145: fix security issue 145

--- src/main/java/net/fashiongo/webadmin/data/model/buyer/GetAdminRetailerParameter.java
+++ src/main/java/net/fashiongo/webadmin/data/model/buyer/GetAdminRetailerParameter.java
@@ -2,11 +2,16 @@ package net.fashiongo.webadmin.data.model.buyer;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import net.fashiongo.webadmin.controller.validator.SQLInjectionSafeWithKeywordsFilter;
 
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 
 @Getter
 public class GetAdminRetailerParameter {
+
+	private static final String ALLOW_PATTERN = "^$|[a-zA-Z0-9\\s =/:!&,-.?_\']+$";
+	private static final String ALLOW_PATTERN_MESSAGE = "Special character not allowed";
 
 	@JsonProperty("pagenum")
 	private Integer pagenum;
@@ -18,9 +23,13 @@ public class GetAdminRetailerParameter {
 	private Integer retailerid;
 
 	@JsonProperty("orderby")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String orderby;
 
 	@JsonProperty("userid")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String userid;
 
 	@JsonProperty("useridpartialmatch")
@@ -48,63 +57,101 @@ public class GetAdminRetailerParameter {
 	private Boolean o;
 
 	@JsonProperty("currentstatus")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String currentstatus;
 
 	@JsonProperty("location")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String location;
 
 	@JsonProperty("state")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String state;
 
 	@JsonProperty("country")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String country;
 
 	@JsonProperty("showid")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String showid;
 
 	@JsonProperty("registerfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String registerfrom;
 
 	@JsonProperty("registerto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String registerto;
 
 	@JsonProperty("logincountfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String logincountfrom;
 
 	@JsonProperty("logincountto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String logincountto;
 
 	@JsonProperty("loginfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String loginfrom;
 
 	@JsonProperty("loginto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String loginto;
 
 	@JsonProperty("ordercountfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String ordercountfrom;
 
 	@JsonProperty("ordercountto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String ordercountto;
 
 	@JsonProperty("checkoutfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String checkoutfrom;
 
 	@JsonProperty("checkoutto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String checkoutto;
 
 	@JsonProperty("ordervendorcountfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String ordervendorcountfrom;
 
 	@JsonProperty("ordervendorcountto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String ordervendorcountto;
 
 	@JsonProperty("wholesalerid")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String wholesalerid;
 
 	@JsonProperty("csv")
 	private Boolean csv;
 
 	@JsonProperty("companyname")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String companyname;
 
 	@JsonProperty("companynamepartialmatch")
@@ -114,21 +161,29 @@ public class GetAdminRetailerParameter {
 	private Boolean companynamestartswith;
 
 	@JsonProperty("firstname")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String firstname;
 
 	@JsonProperty("firstnamepartialmatch")
 	private Boolean firstnamepartialmatch;
 
 	@JsonProperty("lastname")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String lastname;
 
 	@JsonProperty("lastnamepartialmatch")
 	private Boolean lastnamepartialmatch;
 
 	@JsonProperty("orderamountfrom")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String orderamountfrom;
 
 	@JsonProperty("orderamountto")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String orderamountto;
 	
 	@JsonProperty("buyerclass")
