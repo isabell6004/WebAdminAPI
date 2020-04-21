592695f11113e994af5eae43465684bb0007e12b
Andy Min <andy.min@nhnglobal.com>
2020-04-21 13:33:52 -0700

    [Andy] security/146: fix security issue 146, 147

--- src/main/java/net/fashiongo/webadmin/controller/advice/RestControllerExceptionHandler.java
+++ src/main/java/net/fashiongo/webadmin/controller/advice/RestControllerExceptionHandler.java
@@ -1,10 +1,8 @@
 package net.fashiongo.webadmin.controller.advice;
 
 import lombok.extern.slf4j.Slf4j;
-import net.fashiongo.webadmin.controller.MessageController;
-import net.fashiongo.webadmin.controller.StaticController;
-import net.fashiongo.webadmin.controller.StaticKpiController;
-import net.fashiongo.webadmin.controller.WAPaymentController;
+import net.fashiongo.webadmin.controller.*;
+import net.fashiongo.webadmin.controller.vendor.VendorCreditCardController;
 import net.fashiongo.webadmin.utility.JsonResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +18,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.stream.Collectors;
 
 @Slf4j
-@RestControllerAdvice(assignableTypes = {WAPaymentController.class, MessageController.class, StaticController.class, StaticKpiController.class})
+@RestControllerAdvice(assignableTypes = {WAPaymentController.class, MessageController.class,
+        StaticController.class, StaticKpiController.class, BuyerController.class, VendorCreditCardController.class})
 public class RestControllerExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
--- src/main/java/net/fashiongo/webadmin/model/pojo/vendor/parameter/GetVendorCreditCardListParameter.java
+++ src/main/java/net/fashiongo/webadmin/model/pojo/vendor/parameter/GetVendorCreditCardListParameter.java
@@ -2,11 +2,14 @@ package net.fashiongo.webadmin.model.pojo.vendor.parameter;
 
 import java.io.Serializable;
 
+import net.fashiongo.webadmin.controller.validator.SQLInjectionSafeWithKeywordsFilter;
 import org.apache.commons.lang3.StringUtils;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.Pattern;
 
 /**
  * 
@@ -15,9 +18,14 @@ import io.swagger.annotations.ApiModelProperty;
  */
 public class GetVendorCreditCardListParameter implements Serializable {
 	private static final long serialVersionUID = 1L;
+
+	private static final String ALLOW_PATTERN = "^$|[a-zA-Z0-9\\s =/:!&,-.?_\']+$";
+	private static final String ALLOW_PATTERN_MESSAGE = "Special character not allowed";
 	
 	@ApiModelProperty(hidden=true)
 	@JsonProperty("orderby")
+	@SQLInjectionSafeWithKeywordsFilter
+	@Pattern(regexp = ALLOW_PATTERN, message = ALLOW_PATTERN_MESSAGE)
 	private String orderBy;
 
 	public String getOrderBy() {
@@ -27,6 +35,5 @@ public class GetVendorCreditCardListParameter implements Serializable {
 	public void setOrderBy(String orderBy) {
 		this.orderBy = orderBy;
 	}
-	
-	
+
 }
