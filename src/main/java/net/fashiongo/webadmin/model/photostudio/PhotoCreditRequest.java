11975f8e37a240f3319cf0659a75a21facce8730
moonseo.kim <moonseo.kim@nhn.com>
2020-04-21 17:18:56 +0900

    [Moonseo Kim] add localDateTime mapping in PhotoCreditRequest

--- src/main/java/net/fashiongo/webadmin/model/photostudio/PhotoCreditRequest.java
+++ src/main/java/net/fashiongo/webadmin/model/photostudio/PhotoCreditRequest.java
@@ -3,14 +3,21 @@ package net.fashiongo.webadmin.model.photostudio;
 import lombok.Getter;
 import lombok.Setter;
 import net.fashiongo.common.conversion.LocalDateTimeConverter;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.tomcat.jni.Local;
 
 import javax.persistence.Convert;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Setter
 @Getter
 public class PhotoCreditRequest {
+    private static final String FROM_TIME = " 00:00:00";
+    private static final String TO_TIME = " 23:59:59";
+    private static final String DATE_PATTERN_YYYYMMDDHHMMSS = "yyyy-MM-dd HH:mm:ss";
+
     private String photoCreditReason;
     private Integer wholeSalerID;
     private BigDecimal photoCreditAmount;
@@ -18,6 +25,16 @@ public class PhotoCreditRequest {
     @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime fromDate;
 
+    public void setFromDate(String fromDate) {
+        String date = StringUtils.isNotEmpty(fromDate) ? fromDate + FROM_TIME : null;
+        this.fromDate = StringUtils.isNotEmpty(date) ? LocalDateTime.parse(date, DateTimeFormatter.ofPattern(DATE_PATTERN_YYYYMMDDHHMMSS)) : null;
+    }
+
     @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime toDate;
+
+    public void setToDate(String toDate) {
+        String date = StringUtils.isNotEmpty(toDate) ? toDate + TO_TIME : null;
+        this.toDate = StringUtils.isNotEmpty(date) ? LocalDateTime.parse(date, DateTimeFormatter.ofPattern(DATE_PATTERN_YYYYMMDDHHMMSS)) : null;
+    }
 }
