fc85f7aacbc4c346dfd880046db1362ecee7f01f
[Kenny] Kyungwoo Hyun <kyungwoo.hyun@nhnglobal.com>
2020-05-06 14:35:47 -0700

    [Kenny] #security/122: add server-side verification of CouponCreateInput

--- src/main/java/net/fashiongo/webadmin/model/primary/coupon/command/CouponCreateInput.java
+++ src/main/java/net/fashiongo/webadmin/model/primary/coupon/command/CouponCreateInput.java
@@ -4,7 +4,10 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import net.fashiongo.webadmin.exception.coupon.InvalidInputCouponException;
+
+import java.time.LocalDateTime;
 
 @Data
 public class CouponCreateInput extends CouponCommonInput {
@@ -15,8 +18,22 @@ public class CouponCreateInput extends CouponCommonInput {
 
     public void validateCouponCreateInput() {
 
-        if (this.getStartDate() != null && this.getStartDate().isAfter(this.getEndDate())) {
-            throw new InvalidInputCouponException("Invalid period");
+        if (this.getStartDate() != null) {
+            if (this.getStartDate().isBefore(LocalDateTime.now())) {
+                throw new InvalidInputCouponException("startDate should be after today.");
+            }
+            if (this.getEndDate() != null && this.getStartDate().isAfter(this.getEndDate())) {
+                throw new InvalidInputCouponException("endDate should be after startDate.");
+            }
+        }
+
+        if (this.getIssueStartDate() != null) {
+            if (this.getIssueStartDate().isBefore(LocalDateTime.now())) {
+                throw new InvalidInputCouponException("issueStartDate should be after today.");
+            }
+            if (this.getIssueEndDate() != null && this.getIssueStartDate().isAfter(this.getIssueEndDate())) {
+                throw new InvalidInputCouponException("issueEndDate should be after issueStartDate.");
+            }
         }
 
         if (this.getIsCodeUsed() && this.couponCode == null) {
