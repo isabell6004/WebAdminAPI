4ceecaeea6a03087f1e1a2faf2063663e90e0993
[Kenny] Kyungwoo Hyun <kyungwoo.hyun@nhnglobal.com>
2020-05-11 18:16:09 -0700

    [Kenny] security/122: add server-side verification of CouponUpdateInput

--- src/main/java/net/fashiongo/webadmin/model/primary/coupon/command/CouponUpdateInput.java
+++ src/main/java/net/fashiongo/webadmin/model/primary/coupon/command/CouponUpdateInput.java
@@ -5,6 +5,7 @@ import net.fashiongo.webadmin.exception.coupon.InvalidInputCouponException;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Data
 public class CouponUpdateInput extends CouponCommonInput {
@@ -16,9 +17,26 @@ public class CouponUpdateInput extends CouponCommonInput {
     private @Valid CouponCodeUpdateInput couponCode;
 
     public void validateCouponRequest() {
+        if (this.getStartDate() != null) {
+            if (this.getStartDate().isBefore(LocalDateTime.now())) {
+                throw new InvalidInputCouponException("startDate should be after today.");
+            }
+            if (this.getEndDate() != null && this.getStartDate().isAfter(this.getEndDate())) {
+                throw new InvalidInputCouponException("endDate should be after startDate.");
+            }
+        }
 
-        if (this.getStartDate() != null && this.getStartDate().isAfter(this.getEndDate())) {
-            throw new InvalidInputCouponException("Invalid period");
+        if (this.getIssueStartDate() != null) {
+            if (this.getIssueStartDate().isBefore(LocalDateTime.now())) {
+                throw new InvalidInputCouponException("issueStartDate should be after today.");
+            }
+            if (this.getIssueEndDate() != null && this.getIssueStartDate().isAfter(this.getIssueEndDate())) {
+                throw new InvalidInputCouponException("issueEndDate should be after issueStartDate.");
+            }
+        }
+
+        if (this.getIsCodeUsed() && this.couponCode == null) {
+            throw new InvalidInputCouponException("No coupon code");
         }
     }
 }
