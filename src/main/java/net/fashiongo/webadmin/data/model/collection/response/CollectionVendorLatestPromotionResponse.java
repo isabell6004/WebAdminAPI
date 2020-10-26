c20d03b91d62bb591c0fb4cfa2fdb55ddd6dd271
Hee-Sung Park <heesung.park@nhnent.com>
2020-10-25 18:47:42 -0700

    [Hee-Sung Park] feature/collection-promotion-renewal: add promotion toDate, isExpired

--- src/main/java/net/fashiongo/webadmin/data/model/collection/response/CollectionVendorLatestPromotionResponse.java
+++ src/main/java/net/fashiongo/webadmin/data/model/collection/response/CollectionVendorLatestPromotionResponse.java
@@ -1,11 +1,21 @@
 package net.fashiongo.webadmin.data.model.collection.response;
 
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Getter
+import java.time.LocalDateTime;
+
 public class CollectionVendorLatestPromotionResponse {
 
+    @JsonProperty("vendorId")
     private int vendorId;
 
+    @JsonProperty("promotionName")
     private String promotionName;
+
+    @JsonProperty("toDate")
+    private LocalDateTime toDate;
+
+    // compare with server local time
+    @JsonProperty("isExpired")
+    private boolean isExpired;
 }
